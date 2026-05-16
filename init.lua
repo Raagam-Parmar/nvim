3c23db0c9d612ab25c6744ec530adf3f0c352279
Raagam Parmar <raagamparmar@gmail.com>
2026-05-16 19:43:10 +0530

    added plugin folke/snacks.nvim.

--- init.lua
+++ init.lua
@@ -891,6 +891,16 @@ require('lazy').setup({
     end,
   },
 
+  {
+    'folke/snacks.nvim',
+    priority = 1000,
+    lazy = false,
+    ---@type snacks.Config
+    opts = {
+      input = { enabled = true },
+    },
+  },
+
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
