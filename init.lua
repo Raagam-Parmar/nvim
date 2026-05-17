26b7140d03d03a0afa63cb837d2667e78d6feee4
Raagam Parmar <raagamparmar@gmail.com>
2026-05-17 22:41:05 +0530

    disabled format-on-save for ocaml menhir (.mly) files.

--- init.lua
+++ init.lua
@@ -697,7 +697,10 @@ require('lazy').setup({
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true } -- TODO Disable autoformatting for Menhir files
+        local ext = vim.fn.expand '%:e'
+        if ext == 'mly' then return nil end
+
+        local disable_filetypes = { c = true, cpp = true }
         if disable_filetypes[vim.bo[bufnr].filetype] then
           return nil
         else
